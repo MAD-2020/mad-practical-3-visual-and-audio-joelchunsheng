3c465456025369ed76315b828e8c456b07dbdc3e
Joel Yeo <joel.chunsheng@gmail.com>
2020-05-07 03:44:03 +0800

    add video and audio

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,13 +2,33 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Play video
+        videoView = findViewById(R.id.videoView);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
+        videoView.start();
+
+        // Play audio
+        MediaPlayer media = MediaPlayer.create(this, R.raw.baby_laughing);
+        media.start();
+
+//        once done with the playback
+//        media.release();
+//        media = null;
+
+
     }
 }
